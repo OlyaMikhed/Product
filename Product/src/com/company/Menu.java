a9c757c62ac4a15d04b51ef248526ae5bd5fb171
OlyaMikhed <86907873+OlyaMikhed@users.noreply.github.com>
2021-07-04 12:52:53 +0300

    overove

---
+++ Product/src/com/company/Menu.java
@@ -0,0 +1,86 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu {
+    private List<Product> list=new ArrayList<>();
+
+    public Menu(List<Product> list) {
+        this.list = list;
+    }
+    public Menu(){
+    }
+
+    private int count=114;
+    public void additionProduct() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите имя, цену и категорию товара");
+        Product product = new Product(count++, sc.next(), sc.nextDouble(), sc.next());
+        list.add(product);
+    }
+
+    public void findProduct() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите id продукта");
+        int x = sc.nextInt();
+        boolean y=true;
+        for (int i = 0; i < list.size(); i++) {
+            if (x == list.get(i).getId()) {
+                y=false;
+                System.out.println(list.get(i));
+            }
+        }
+        if (y){System.out.println("Продукта с таким id нет в наличии");}
+    }
+
+    public void deleteProduct() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите id продукта");
+        int x = sc.nextInt();
+        boolean y=true;
+        for (int i = 0; i < list.size(); i++) {
+            if (x == list.get(i).getId()) {
+                y=false;
+                list.remove(i);
+                System.out.println("Продукт удален");
+            }
+        }
+        if (y){System.out.println("Продукта с таким id нет в наличии");}
+    }
+
+    public void printList() {
+       for (int i = 0; i < list.size(); i++)
+           System.out.println(list.get(i));
+    }
+
+    public void menu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Выберите позицию:\n1. Добавить продукт в список\n2. Найти продукт по id\n3. Удалить продукт по id\n4. Показать список продуктов\n5. Выход");
+        int change = sc.nextInt();
+        switch (change) {
+            case 1:
+                additionProduct();
+                menu();
+                break;
+            case 2:
+                findProduct();
+                menu();
+                break;
+            case 3:
+                deleteProduct();
+                menu();
+                break;
+            case 4:
+                printList();
+                menu();
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("вы ввели неверное значение");
+                menu();
+        }
+    }
+}
---
+++ Product/src/com/company/Product.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private String category;
+    private double discount;
+
+    Product(int id, String name, double price, String category){
+        this.id=id;
+        this.name=name;
+        this.price=price;
+        this.category=category;
+    }
+
+    public Product() {
+    }
+
+    public int getId(){return id;}
+    public String getName(){return name;}
+    public double getPrice(){return price;}
+    public String getCategory (){return category;}
+    public double getDiscount() { return discount;}
+
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setPrice(double price) { this.price = price; }
+    public void setCategory(String category) { this.category = category;}
+    public void setDiscount(double discount) {this.discount=discount;}
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", discount=" + discount +
+                '}';
+    }
+}
