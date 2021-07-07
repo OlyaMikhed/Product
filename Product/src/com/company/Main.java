de907d0960b5071fc80d1d5e49fd68743e1bee60
Ola <olyam010297@gmail.com>
2021-07-07 21:51:18 +0300

    коллекция вынесена в новый класс

--- Product/src/com/company/Main.java
+++ Product/src/com/company/Main.java
@@ -1,16 +1,9 @@
 package com.company;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Product> l=new ArrayList<>();
-        l.add(new Product(111, "пирожное", 150.2, "еда"));
-        l.add(new Product(112, "конфета", 180.5, "еда"));
-        l.add(new Product(113, "торт", 63.2, "еда"));
-
-        Menu list=new Menu(l);
+        Menu list=new Menu();
         list.menu();
     }
 }
